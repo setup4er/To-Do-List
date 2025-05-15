c78e4f092b47c00017e4c662b780f474b43f380f
Ivan <ivan.andreevich000@gmail.com>
2025-05-15 04:03:14 +0300

    Refactoring structure of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,15 @@
 import { useState } from "react";
-import { ToDoSection } from "./components/ToDoSection";
-
-import './styles/App.css'
+import ToDoWrapper from "./components/ToDoWrapper";
 
 function App() {
   const [value, setValue] = useState('');
   const [todoListArray, setTodoListArray] = useState([]);
 
-  const onChangeInputHandler = (event) => {
-    setValue(event.target.value);
-  };
-
-  const onClickTodoHandler = () => {
-    setTodoListArray([...todoListArray, { text: value ? value : "( none )", isPassed: false }]);
-    setValue('');
-  };
-
-  const onClickDeleteHandler = (index) => { 
-    setTodoListArray(todoListArray.filter((_,i) => i !== index))
-  }
-
-  const toggleTodoStatus = (index) => {
-    setTodoListArray(
-      todoListArray.map((todo, i) =>
-        i === index ? { ...todo, isPassed: !todo.isPassed } : todo
-      )
-    );
-  };
-
   return (
-    <div className="wrapper">
-      <div className="title">
-        <h1>To-do list</h1>
-      </div>
-      <div className="input-section">
-        <input className="input-section-item" type="text" value={value} onChange={onChangeInputHandler} />
-        <input className="input-section-item" type="button" value="Add to-do" onClick={onClickTodoHandler} />
-      </div>
-      <div className="todo-section">
-        <ToDoSection 
-          onClickDeleteHandler = {onClickDeleteHandler} 
-          todoListArray={todoListArray} 
-          toggleTodoStatus={toggleTodoStatus}/>
-      </div>
-    </div>
+    <ToDoWrapper value={value} 
+    todoListArray={todoListArray}
+    setValue={setValue}
+    setTodoListArray={setTodoListArray}/>
   );
 }
 
---
+++ src/components/ToDoWrapper.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+
+import { ToDoSection } from "./ToDoSection";
+import '../styles/ToDoWrapper.css'
+
+function ToDoWrapper({value, todoListArray, setTodoListArray, setValue}) {
+
+  const onChangeInputHandler = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onClickTodoHandler = () => {
+    setTodoListArray([...todoListArray, { text: value ? value : "( none )", isPassed: false }]);
+    setValue('');
+  };
+
+  const onClickDeleteHandler = (index) => { 
+    setTodoListArray(todoListArray.filter((_,i) => i !== index))
+  }
+
+  const toggleTodoStatus = (index) => {
+    setTodoListArray(
+      todoListArray.map((todo, i) =>
+        i === index ? { ...todo, isPassed: !todo.isPassed } : todo
+      )
+    );
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="title">
+        <h1>To-do list</h1>
+      </div>
+      <div className="input-section">
+        <input className="input-section-item" type="text" value={value} onChange={onChangeInputHandler} />
+        <input className="input-section-item" type="button" value="Add to-do" onClick={onClickTodoHandler} />
+      </div>
+      <div className="todo-section">
+        <ToDoSection 
+          onClickDeleteHandler = {onClickDeleteHandler} 
+          todoListArray={todoListArray} 
+          toggleTodoStatus={toggleTodoStatus}/>
+      </div>
+    </div>
+  );
+}
+
+export default ToDoWrapper;
