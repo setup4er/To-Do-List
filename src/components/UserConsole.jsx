be6764ad1566f83d33fa2b6f49e3d9149f343cbf
Ivan <ivan.andreevich000@gmail.com>
2025-05-15 10:23:38 +0300

    Added autoscrollit in console + Diff styles fixes

--- src/components/UserConsole.jsx
+++ src/components/UserConsole.jsx
@@ -1,29 +1,35 @@
-import { useState } from "react";
-
+import { useState, useRef, useEffect } from "react";
 import '../styles/UserConsole.css';
 
 export const UserConsole = () => {
   const [logs, setLogs] = useState([]);
   const [inputValue, setInputValue] = useState("");
+  const consoleLogRef = useRef(null); // Создаём реф для прокрутки
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
   };
 
   const handleEnterPress = (event) => {
-    let time = new Date();
+    const time = new Date();
     const [hours, minutes, seconds] = [String(time.getHours()), String(time.getMinutes()), String(time.getSeconds())];
+    const timeLog = `[${hours.padStart(2, '0')}:${minutes.padStart(2, '0')}:${seconds.padStart(2, '0')}]`;
 
     if (event.key === "Enter" && inputValue.trim()) {
-      setLogs([...logs, `[${hours.padStart(2, '0')}:${minutes.padStart(2, '0')}:${seconds.padStart(2, '0')}] ${inputValue}`]);
-      
+      setLogs((prevLogs) => [...prevLogs, `${timeLog} ${inputValue}`]);
       setInputValue("");
     }
   };
 
+  useEffect(() => {
+    if (consoleLogRef.current) {
+      consoleLogRef.current.scrollTop = consoleLogRef.current.scrollHeight;
+    }
+  }, [logs]); // Прокрутка вниз при изменении логов
+
   return (
     <div className="console-container">
-      <div className="console-log">
+      <div className="console-log" ref={consoleLogRef}>
         {logs.map((log, index) => (
           <div key={index} className="console-message">{log}</div>
         ))}
